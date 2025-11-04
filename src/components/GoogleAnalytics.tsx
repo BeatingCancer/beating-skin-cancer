41f6facf03276273a7170025a63359eee8ecbc3b
Beating Cancer <zenonteam55@gmail.com>
2025-11-04 09:06:10 -0800

    Updated GoogleAnalytics.tsx

--- src/components/GoogleAnalytics.tsx
+++ src/components/GoogleAnalytics.tsx
@@ -1,26 +1,37 @@
-import { Helmet } from 'react-helmet';
+import { useEffect } from 'react';
+
+declare global {
+  interface Window {
+    dataLayer: any[];
+    gtag: (...args: any[]) => void;
+  }
+}
 
 export default function GoogleAnalytics() {
   const measurementId = import.meta.env.VITE_GA_MEASUREMENT_ID;
 
-  if (!measurementId) {
-    return null;
-  }
+  useEffect(() => {
+    if (!measurementId) return;
 
-  return (
-    <Helmet>
-      <script
-        async
-        src={`https://www.googletagmanager.com/gtag/js?id=${measurementId}`}
-      />
-      <script>
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', '${measurementId}');
-        `}
-      </script>
-    </Helmet>
-  );
+    const script1 = document.createElement('script');
+    script1.async = true;
+    script1.src = `https://www.googletagmanager.com/gtag/js?id=${measurementId}`;
+    document.head.appendChild(script1);
+
+    const script2 = document.createElement('script');
+    script2.innerHTML = `
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', '${measurementId}');
+    `;
+    document.head.appendChild(script2);
+
+    return () => {
+      document.head.removeChild(script1);
+      document.head.removeChild(script2);
+    };
+  }, [measurementId]);
+
+  return null;
 }
